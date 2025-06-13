34a0338a646b2f7831fc6cb7d0efc62d9ea0ba98
megan.ayucedar <megan.ayucedar@pajakku.com>
2025-06-13 17:18:49 +0700

    fix: fixing background layout

--- src/components/Greenhouse.js
+++ src/components/Greenhouse.js
@@ -1,21 +1,24 @@
 import React from "react";
 
 const Greenhouse = ({ children, imagePath }) => {
-    return (
-         <div
-            style={{
-                width: '100%',
-                height: '100%',
-                backgroundImage: `url(${encodeURI(imagePath)})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'top center',
-                backgroundRepeat: 'no-repeat',
-                position: 'relative',
-            }}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "600px",
+        aspectRatio: "3 / 5", // atau paddingTop kalau belum support
+        backgroundColor: "blue", // biar kelihatan dulu
+        backgroundImage: imagePath ? `url(${encodeURI(imagePath)})` : "none",
+        backgroundSize: "cover",
+        backgroundPosition: "top center",
+        backgroundRepeat: "no-repeat",
+        position: "relative",
+        margin: "0 auto",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Greenhouse;
